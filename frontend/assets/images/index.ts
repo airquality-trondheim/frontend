79d288cd0498460ce5798eebd7ff9fe412ee0ea1
sunnivbl <sunnivbl@stud.ntnu.no>
2020-09-08 12:36:51 +0200

    #2 - Create template for further development

---
+++ frontend/assets/images/index.ts
@@ -0,0 +1,3 @@
+
+
+export const death = require('./death.png');
--- frontend/screens/TabOneScreen.tsx
+++ frontend/screens/TabOneScreen.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { StyleSheet } from 'react-native';
-
+import { death } from '../assets/images'
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 
@@ -8,6 +8,7 @@ export default function TabOneScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Air Pollution App</Text>
+      <img src={death} style={{width:40, height:40}}/>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="/screens/TabOneScreen.tsx" />
     </View>
