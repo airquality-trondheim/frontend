fb966d02878d0f8a726412689e30fe310dcdc628
sunnivbl <sunnivbl@stud.ntnu.no>
2020-09-09 19:07:30 +0200

    Add dependencies and readme

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -17,7 +17,6 @@ export default function App() {
       <SafeAreaProvider>
         <Navigation colorScheme={colorScheme} />
         <StatusBar />
-        Hei
       </SafeAreaProvider>
     );
   }
--- frontend/components/EditScreenInfo.tsx
+++ frontend/components/EditScreenInfo.tsx
@@ -14,28 +14,28 @@ export default function EditScreenInfo({ path }: { path: string }) {
           style={styles.getStartedText}
           lightColor="rgba(0,0,0,0.8)"
           darkColor="rgba(255,255,255,0.8)">
-          Open up the code for this screen:
+          Current air pollution level: 
         </Text>
 
         <View
           style={[styles.codeHighlightContainer, styles.homeScreenFilename]}
           darkColor="rgba(255,255,255,0.05)"
           lightColor="rgba(0,0,0,0.05)">
-          <MonoText>{path}</MonoText>
+          <MonoText>High</MonoText>
         </View>
 
         <Text
           style={styles.getStartedText}
           lightColor="rgba(0,0,0,0.8)"
           darkColor="rgba(255,255,255,0.8)">
-          Change any of the text, save the file, and your app will automatically update.
+          Do not go outside
         </Text>
       </View>
 
       <View style={styles.helpContainer}>
         <TouchableOpacity onPress={handleHelpPress} style={styles.helpLink}>
           <Text style={styles.helpLinkText} lightColor={Colors.light.tint}>
-            Tap here if your app doesn't automatically update after making changes
+            If you need help, please press here
           </Text>
         </TouchableOpacity>
       </View>
