a5f4e55f2acc36691b45fae4e3a2a9705426b7e8
kmidtlid3@gmail.com <kmidtlid3@gmail.com>
2020-09-18 13:35:56 +0200

    #5 - Add auth to access TabTwoScreen

--- frontend/screens/TabTwoScreen.tsx
+++ frontend/screens/TabTwoScreen.tsx
@@ -1,18 +1,52 @@
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, TouchableOpacity } from 'react-native';
+import Colors from '../constants/Colors';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 
-export default function TabTwoScreen() {
+import Amplify from 'aws-amplify';
+import config from '../aws-exports';
+import { withAuthenticator } from 'aws-amplify-react-native';
+import { Auth } from 'aws-amplify';
+
+Amplify.configure({
+  ...config,
+  Analytics: {
+    disabled: true,
+  },
+});
+
+async function signOut() {
+  try {
+    await Auth.signOut();
+  } catch (error) {
+    console.log('error signing out: ', error);
+  }
+}
+
+function TabTwoScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabTwoScreen.tsx" />
+      <Text style={styles.title}>
+        Hello {Auth.Credentials.Auth.user.username}
+      </Text>
+      <View
+        style={styles.separator}
+        lightColor='#eee'
+        darkColor='rgba(255,255,255,0.1)'
+      />
+      <EditScreenInfo path='/screens/TabTwoScreen.tsx' />
+      <View>
+        <TouchableOpacity onPress={signOut}>
+          <Text lightColor={Colors.light.tint}>Click here to log out</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
+
+export default withAuthenticator(TabTwoScreen);
 
 const styles = StyleSheet.create({
   container: {
