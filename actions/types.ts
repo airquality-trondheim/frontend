a4ce76f2a1693d9c6604ffc4effe10467d07d00e
mirree <32240143+mirree@users.noreply.github.com>
2020-10-09 14:00:46 +0200

    #48 - Add user's points to redux

--- actions/types.ts
+++ actions/types.ts
@@ -4,7 +4,7 @@ import {
   UserRanking,
 } from '../types/_types';
 
-export type RootAction = LeaderboardActionTypes;
+export type RootAction = LeaderboardActionTypes | PointsActionTypes;
 
 // Leaderboard
 export const GET_LEADERBOARD = 'GET_LEADERBOARD';
@@ -13,6 +13,11 @@ export const GET_USERRANKING = 'GET_USERRANKING';
 type GetLeaderboardAction = {
   type: typeof GET_LEADERBOARD;
   data: LeaderboardElement[];
+};
+
+type GetUserRankingAction = {
+  type: typeof GET_USERRANKING;
+  userRanking: UserRanking;
 };
 
 export type LeaderboardActionTypes =
@@ -28,7 +33,13 @@ type GetAirQualityDataAction = {
 };
 
 export type MapActionTypes = GetAirQualityDataAction;
-type GetUserRankingAction = {
-  type: typeof GET_USERRANKING;
-  userRanking: UserRanking;
+
+// Points
+export const GET_USERPOINTS = 'GET_USERPOINTS';
+
+type GetUserPointsAction = {
+  type: typeof GET_USERPOINTS;
+  points: number;
 };
+
+export type PointsActionTypes = GetUserPointsAction;
--- reducers/index.ts
+++ reducers/index.ts
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 
 import leaderboardReducer from './leaderboardReducer';
 import mapReducer from './mapReducer';
+import pointsReducer from './pointsReducer';
 
 export const rootReducer = combineReducers({
   // reducerName: reducerImported
   leaderboard: leaderboardReducer,
   map: mapReducer,
+  points: pointsReducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
