87d650ef991ee939957cd96d79d611b5b04c05b0
mirree <32240143+mirree@users.noreply.github.com>
2020-10-09 14:04:53 +0200

    #50 - Add user's points to the competition page

--- screens/CompetitionPage.tsx
+++ screens/CompetitionPage.tsx
@@ -1,18 +1,48 @@
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import React, { useEffect } from 'react';
+import { View, StyleSheet, Text } from 'react-native';
 import CompetitionPageCarousel from '../components/CompetitionPageCarousel';
 import { CarouselItem } from '../components/CarouselItem';
+import { connect } from 'react-redux';
+import { Dispatch } from 'redux';
+import { RootAction } from '../actions/types';
+import { RootState } from '../reducers';
+import { getUserPoints } from '../actions/pointsActions';
 
-function CompetitionPage() {
+type CompetitionPageProps = ReturnType<typeof mapStateToProps> &
+  ReturnType<typeof mapDispatchToProps>;
+
+function CompetitionPage(props: CompetitionPageProps) {
+  const { points, fetchPoints } = props;
+
+  useEffect(() => {
+    // TODO: Change to user's ID after log in is done
+    fetchPoints('5f6dde0d71a2bf3507462942');
+  }, [fetchPoints]);
+
   return (
     <View style={styles.screenStyle}>
-      <CarouselItem headerText="Hei" />
+      <CarouselItem headerText="Hei">
+        <Text>Poeng: {points > -1 ? points : 'Kunne ikke hente poeng'}</Text>
+      </CarouselItem>
       <CompetitionPageCarousel />
     </View>
   );
 }
+const mapDispatchToProps = (dispatch: Dispatch<RootAction>) => {
+  return {
+    fetchPoints: (userID: string) => {
+      getUserPoints(userID, dispatch);
+    },
+  };
+};
 
-export default CompetitionPage;
+const mapStateToProps = (state: RootState) => {
+  return {
+    points: state.points.points,
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(CompetitionPage);
 
 const styles = StyleSheet.create({
   screenStyle: {
