3567567bb6ffb5c329ff5193723b93b12908c110
mirree <32240143+mirree@users.noreply.github.com>
2020-10-10 15:14:40 +0200

    #50 - Add progress circle and display of avatar and level

--- screens/CompetitionPage.tsx
+++ screens/CompetitionPage.tsx
@@ -1,62 +1,23 @@
-import React, { useEffect } from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
 import CompetitionPageCarousel from '../components/CompetitionPageCarousel';
-import { connect } from 'react-redux';
-import { Dispatch } from 'redux';
-import { RootAction } from '../actions/types';
-import { RootState } from '../reducers';
-import { getUserPoints } from '../actions/pointsActions';
-import { height, width } from '../constants/Layout';
-import { CAROUSELITEM } from '../constants/Colors';
+import ProgressCircle from '../components/ProgressCircle';
 
-type CompetitionPageProps = ReturnType<typeof mapStateToProps> &
-  ReturnType<typeof mapDispatchToProps>;
-
-function CompetitionPage(props: CompetitionPageProps) {
-  const { points, fetchPoints } = props;
-
-  useEffect(() => {
-    // TODO: Change to user's ID after log in is done
-    fetchPoints('5f6dde0d71a2bf3507462942');
-  }, [fetchPoints]);
-
+function CompetitionPage() {
   return (
     <View style={styles.screenStyle}>
-      <View style={styles.card}>
-        <Text>Poeng: {points > -1 ? points : 'Kunne ikke hente poeng'}</Text>
-      </View>
+      <ProgressCircle />
       <CompetitionPageCarousel />
     </View>
   );
 }
-const mapDispatchToProps = (dispatch: Dispatch<RootAction>) => {
-  return {
-    fetchPoints: (userID: string) => {
-      getUserPoints(userID, dispatch);
-    },
-  };
-};
 
-const mapStateToProps = (state: RootState) => {
-  return {
-    points: state.points.points,
-  };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(CompetitionPage);
+export default CompetitionPage;
 
 const styles = StyleSheet.create({
   screenStyle: {
     flex: 1,
     justifyContent: 'space-evenly',
     alignItems: 'center',
-  },
-  card: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: width * 0.6,
-    height: height * 0.1,
-    backgroundColor: CAROUSELITEM,
-    borderRadius: 20,
   },
 });
