5f322186741d11f17161b5d718bed70559537b59
mathildehaugum <mathilde_haugum@hotmail.com>
2020-10-19 11:48:18 +0200

    #28 - Add subpages for setting page

---
+++ constants/SettingHelpArray.ts
@@ -0,0 +1,45 @@
+const settingHelpArray = [
+  {
+    id: 1,
+    question: 'Hvorfor er det viktig å følge med på luftkvaliteten?',
+    answer: 'Dersom man puster inn mye forurensning kan man bli syk, blabla',
+  },
+  {
+    id: 2,
+    question: 'Hvordan blir luftkvaliteten målt?',
+    answer:
+      'Det er utplassert sensorer som måler viktige faktorer som AQI, blabla',
+  },
+  {
+    id: 3,
+    question: 'Hvor er sensorene plassert?',
+    answer:
+      'Foreløpig er det plassert sensorer på Byåsen, Ranheim, Tiller, Sjetnemarka og Sluppen. Disse kan ses i kartet',
+  },
+  {
+    id: 4,
+    question: 'Hvem kan se hvor ofte jeg har gått til jobb?',
+    answer:
+      'Andre brukere kan ikke se hvor ofte de har gått jobb, men navnet ditt kan være gitt i topplisten.',
+  },
+  {
+    id: 5,
+    question: 'Hvem kan se hvor ofte jeg har gått til jobb?',
+    answer:
+      'Andre brukere kan ikke se hvor ofte de har gått jobb, men navnet ditt kan være gitt i topplisten.',
+  },
+  {
+    id: 6,
+    question: 'Hvem kan se hvor ofte jeg har gått til jobb?',
+    answer:
+      'Andre brukere kan ikke se hvor ofte de har gått jobb, men navnet ditt kan være gitt i topplisten.',
+  },
+  {
+    id: 7,
+    question: 'Hvem kan se hvor ofte jeg har gått til jobb?',
+    answer:
+      'Andre brukere kan ikke se hvor ofte de har gått jobb, men navnet ditt kan være gitt i topplisten.',
+  },
+];
+
+export default settingHelpArray;
