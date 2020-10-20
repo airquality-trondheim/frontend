49347947b414eaac781a15effdb27b0312864b44
gitforl <lukashg@stud.ntnu.no>
2020-10-20 20:12:08 +0200

    updated to utilize new component

--- screens/ProfilePage.tsx
+++ screens/ProfilePage.tsx
@@ -1,14 +1,16 @@
 import { useNavigation } from '@react-navigation/native';
 import { Button, Grid, Row } from 'native-base';
 import React, { useEffect } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 import { connect } from 'react-redux';
 import { getProfileData } from '../actions/profileActions'
 import { RootState } from '../reducers';
 import { Dispatch } from 'redux';
 import { RootAction } from '../actions/types';
-import { LIGHTBLUE, WHITE, DARKRED, BACKGROUNDCOLOR4 } from '../constants/Colors';
+import { LIGHTBLUE, WHITE, DARKRED, BACKGROUNDCOLOR4, BACKGROUNDCOLOR2 } from '../constants/Colors';
 import { height, width } from '../constants/Layout';
+import { SvgUri} from 'react-native-svg';
+import { ProfileTextContainer } from '../components/ProfileTextContainer';
 
 type UserProfileProps = ReturnType<typeof mapStateToProps> &
   ReturnType<typeof mapDispatchToProps>;
@@ -27,23 +29,46 @@ function ProfilePage(props: UserProfileProps) {
 
   const formatProfileNameText = userProfile.username == '0' ? 'Blekk Blekksprut' : userProfile.username;
   
+  const formatMail = userProfile.mail === '0' ? 'fill in mail' : userProfile.mail;
+  const formatTelefon = userProfile.telefon === '0' ? 'fill in mail' : userProfile.telefon;
+  const formatLocation = userProfile.location === '0' ? 'fill in mail' : userProfile.location;
+  const formatBirthdate = userProfile.birthdate === '0' ? 'fill in mail' : userProfile.birthdate;
+  const formatStreet = userProfile.street === '0' ? 'fill in mail' : userProfile.street;  
+  const formatPostalCode = userProfile.postalcode === '0' ? 'fill in mail' : userProfile.postalcode;
 
   return (
     <Grid>
       <Row size={4}>
-        <View style={[styles.centeredView, styles.profileView]}>
-          <View style={styles.upperLeftContainer}>
-            <View style={styles.avatarContainer}>
-
+        <View>
+          <View style={[styles.profileView]}>
+            <View style={styles.upperLeftContainer}>
+              <View style={styles.avatarContainer}>
+                <View style={{width:width*0.2}}>
+                  <SvgUri
+                    uri={userProfile.symbolUrl}
+                  />
+                </View>                
+              </View>
+            </View>
+            <View style={styles.upperRightContainer}>
+              <View style={styles.upperTextContainer}>            
+                <Text style={styles.upperTextFormat}>{formatProfileLevelText}</Text>
+              </View>
+              <View style={styles.upperTextContainer}>         
+                <Text style={styles.upperTextFormat}>{formatProfileNameText}</Text>
+              </View>
             </View>
           </View>
-          <View style={styles.upperRightContainer}>
-            <View style={styles.upperTextContainer}>            
-              <Text style={styles.upperTextFormat}>{formatProfileLevelText}</Text>
-            </View>
-            <View style={styles.upperTextContainer}>         
-              <Text style={styles.upperTextFormat}>{formatProfileNameText}</Text>
-            </View>
+          <View style={[styles.centeredView ,{width:width, height:height*0.40}]}>
+            <ProfileTextContainer text={formatMail} />
+            <ProfileTextContainer text={formatTelefon}/>
+            <ProfileTextContainer text={formatBirthdate}/>
+            <View style={{flexDirection:'row'}}>  
+              <ProfileTextContainer text={formatLocation} outerWidth={width*0.47}/>
+              <View style={styles.minorSpace}/>
+              <ProfileTextContainer text={formatPostalCode} outerWidth={width*0.3}/>
+            </View>  
+            <ProfileTextContainer text={formatStreet}/>
           </View>
         </View>
       </Row>
@@ -159,5 +184,36 @@ const styles = StyleSheet.create({
     height:width*0.32,
     backgroundColor: WHITE,
     borderRadius:width*0.16,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  imageStyle: {
+    width: width*0.3,
+    height: width*0.3,
+  },
+
+  colouredBorder: {
+    borderWidth:width*0.01, 
+    borderRadius:width*0.05, 
+    borderColor:BACKGROUNDCOLOR2, 
+    backgroundColor: 'white', 
+    width:width*0.8, 
+    height:height*0.05,
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginVertical: height*0.01,
+  },
+
+  iconSphere: {
+    width: width*0.05,
+    height: width*0.05,
+    backgroundColor: BACKGROUNDCOLOR2,
+    borderRadius: width*0.05,
+    marginHorizontal: width*0.02
+  },
+
+  minorSpace: {
+    width:width*0.03
   },
 });
