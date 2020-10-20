05204d6966da3a464af5ebd60037d8d676fce235
gitforl <lukashg@stud.ntnu.no>
2020-10-20 15:38:17 +0200

    changed name of component group to be in line

--- components/CompetitionPageCarousel.tsx
+++ components/CompetitionPageCarousel.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ScrollView, View, StyleSheet } from 'react-native';
 import { width, carouselHeight } from '../constants/Layout';
-import AchievementCard from './Achievements/AchievementCard';
+import AchievementCard from './achievements/AchievementCard';
 import { CarouselItem } from './CarouselItem';
 import LeaderboardCardWithModal from './LeaderBoardCardWithModal';
 
