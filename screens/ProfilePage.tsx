427497bc906f3350abbf8ac46e1e29680680fe9a
gitforl <lukashg@stud.ntnu.no>
2020-10-20 15:38:47 +0200

    added profile functionality

--- screens/ProfilePage.tsx
+++ screens/ProfilePage.tsx
@@ -1,16 +1,51 @@
 import { useNavigation } from '@react-navigation/native';
 import { Button, Grid, Row } from 'native-base';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { LIGHTBLUE, WHITE, DARKRED } from '../constants/Colors';
+import { connect } from 'react-redux';
+import { getProfileData } from '../actions/profileActions'
+import { RootState } from '../reducers';
+import { Dispatch } from 'redux';
+import { RootAction } from '../actions/types';
+import { LIGHTBLUE, WHITE, DARKRED, BACKGROUNDCOLOR4 } from '../constants/Colors';
 import { height, width } from '../constants/Layout';
 
-function ProfilePage() {
+type UserProfileProps = ReturnType<typeof mapStateToProps> &
+  ReturnType<typeof mapDispatchToProps>;
+
+function ProfilePage(props: UserProfileProps) {
   const navigation = useNavigation();
+  const {userProfile, fetchUserProfile,} = props;
+
+  useEffect(() => {
+    //No idea how IDs are used, this one was copied from LeaderBoardCardWithModal.tsx  
+    fetchUserProfile('5f6dde0d71a2bf3507462942');
+  }, [fetchUserProfile]);
+
+  const formatProfileLevelText = userProfile.level == 0? 'Nivå 2 - 304 poeng'
+    : 'Nivå ' + userProfile.level + ' - ' + userProfile.points + ' poeng' ;
+
+  const formatProfileNameText = userProfile.username == '0' ? 'Blekk Blekksprut' : userProfile.username;
+  
+
   return (
     <Grid>
       <Row size={4}>
-        <View style={[styles.centeredView, styles.profileView]}></View>
+        <View style={[styles.centeredView, styles.profileView]}>
+          <View style={styles.upperLeftContainer}>
+            <View style={styles.avatarContainer}>
+
+            </View>
+          </View>
+          <View style={styles.upperRightContainer}>
+            <View style={styles.upperTextContainer}>            
+              <Text style={styles.upperTextFormat}>{formatProfileLevelText}</Text>
+            </View>
+            <View style={styles.upperTextContainer}>         
+              <Text style={styles.upperTextFormat}>{formatProfileNameText}</Text>
+            </View>
+          </View>
+        </View>
       </Row>
       <Row size={1}>
         <View style={styles.centeredView}>
@@ -32,7 +67,21 @@ function ProfilePage() {
   );
 }
 
-export default ProfilePage;
+const mapStateToProps = (state: RootState) => {
+  return {
+    userProfile: state.userprofile
+  };
+};
+
+const mapDispatchToProps = (dispatch: Dispatch<RootAction>) => {
+  return{
+    fetchUserProfile: (userID: string) => {
+      getProfileData(userID, dispatch);
+    }
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ProfilePage);
 
 const styles = StyleSheet.create({
   button: {
@@ -44,25 +93,71 @@ const styles = StyleSheet.create({
     elevation: 2,
     alignSelf: 'center',
   },
+
   settingsButton: {
     backgroundColor: LIGHTBLUE,
   },
+
   logOutButton: {
     backgroundColor: DARKRED,
   },
+
   buttonText: {
     color: WHITE,
     fontWeight: 'bold',
     textAlign: 'center',
   },
+
   centeredView: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
   },
+
   profileView: {
     width: width,
     height: 0.25 * height,
     backgroundColor: LIGHTBLUE,
+    flexDirection: 'row',
+  },
+
+  upperTextContainer: {
+    width:width*0.55,
+    height:height*0.04,
+    backgroundColor: WHITE,
+    borderRadius: height*0.02,
+    margin: height*0.015,
+    justifyContent: 'center',
+  },
+
+  upperTextFormat: {
+    marginLeft: width*0.04,
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+
+  marginLeftFour: {
+    marginLeft: width*0.04,
+  },
+
+  upperRightContainer: {
+    width:width*0.61,
+    height:height*0.25,
+    justifyContent:'center',
+    alignItems: 'center',
+  },
+
+  upperLeftContainer: {
+    width:width*0.39,
+    height:height*0.25,
+    justifyContent:'center',
+    alignItems:'center',
+  },
+
+  avatarContainer: {
+    width:width*0.32,
+    height:width*0.32,
+    backgroundColor: WHITE,
+    borderRadius:width*0.16,
   },
 });
