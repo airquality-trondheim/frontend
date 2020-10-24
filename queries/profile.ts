1360961f02bb4f5f213c0aa760221cd74b40ba63
gitforl <lukashg@stud.ntnu.no>
2020-10-24 22:46:43 +0200

    removed unused import

--- queries/profile.ts
+++ queries/profile.ts
@@ -1,4 +1,3 @@
-import createWebpackConfigAsync from '@expo/webpack-config/webpack';
 import { UserProfile } from '../types/_types';
 const endpoint =
   'http://ec2-18-192-82-31.eu-central-1.compute.amazonaws.com/';
