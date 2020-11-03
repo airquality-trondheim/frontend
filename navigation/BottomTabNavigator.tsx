f4c0f6f3939e5700aab1c9f95a0fd136d42af88b
gitforl <lukashg@stud.ntnu.no>
2020-11-03 22:35:22 +0100

    # bottom bar fixed

--- navigation/BottomTabNavigator.tsx
+++ navigation/BottomTabNavigator.tsx
@@ -47,7 +47,7 @@ export default function BottomTabNavigator() {
       initialRouteName="Home"
       tabBarOptions={{
         activeTintColor: WHITE,
-        style: { height: 55 },
+        style: { minheight: 55 },
         labelStyle: { marginBottom: 3 },
       }}
     >
