8ea2311f2a63ed87e3521ce623b62ede53628606
mirree <32240143+mirree@users.noreply.github.com>
2020-11-10 20:29:02 +0100

    #63 - Fix functionality for updating charts on location change

--- screens/AirQualityScreen.tsx
+++ screens/AirQualityScreen.tsx
@@ -3,26 +3,10 @@ import React, { useEffect, useRef, useState } from 'react';
 import AQChart from '../components/airquality/AQChart';
 import { height, width } from '../constants/Layout';
 import LocationDropdown from '../components/LocationDropdown';
-import { RootState } from '../reducers';
-import { Dispatch } from 'redux';
-import { RootAction } from '../actions/types';
-import { getAirQualityDataForLocation } from '../actions/airqualityActions';
-import { connect } from 'react-redux';
 import InfoButton from '../components/InfoButton';
 import AQInfoModal from '../components/airquality/AQInfoModal';
 
-type AirQualityScreenProps = ReturnType<typeof mapStateToProps> &
-  ReturnType<typeof mapDispatchToProps>;
-
-function AirQualityScreen(props: AirQualityScreenProps) {
-  const {
-    currentLocation,
-    AQI,
-    NO2_AQI,
-    PM10_AQI,
-    PM25_AQI,
-    fetchAirQualityData,
-  } = props;
+function AirQualityScreen() {
   const [modalVisible, setModalVisible] = useState(false);
   const unmounted = useRef(false);
 
@@ -31,12 +15,6 @@ function AirQualityScreen(props: AirQualityScreenProps) {
       unmounted.current = true;
     };
   }, []);
-
-  useEffect(() => {
-    if (currentLocation) {
-      fetchAirQualityData(currentLocation.areacode);
-    }
-  }, [fetchAirQualityData, currentLocation]);
 
   const updateModalVisible = () => {
     if (!unmounted.current) {
@@ -69,33 +47,15 @@ function AirQualityScreen(props: AirQualityScreenProps) {
         modalVisible={modalVisible}
         modalOnRequestClose={updateModalVisible}
       />
-      <AQChart {...{ name: 'AQI', nameNumber: '', AQ: AQI }} />
-      <AQChart {...{ name: 'AQI NO', nameNumber: '2', AQ: NO2_AQI }} />
-      <AQChart {...{ name: 'AQI PM', nameNumber: '2.5', AQ: PM10_AQI }} />
-      <AQChart {...{ name: 'AQI PM', nameNumber: '10', AQ: PM25_AQI }} />
+      <AQChart {...{ name: 'AQI', nameNumber: '', AQ: 'AQI' }} />
+      <AQChart {...{ name: 'AQI NO', nameNumber: '2', AQ: 'NO2_AQI' }} />
+      <AQChart {...{ name: 'AQI PM', nameNumber: '2.5', AQ: 'PM10_AQI' }} />
+      <AQChart {...{ name: 'AQI PM', nameNumber: '10', AQ: 'PM25_AQI' }} />
     </ScrollView>
   );
 }
 
-const mapDispatchToProps = (dispatch: Dispatch<RootAction>) => {
-  return {
-    fetchAirQualityData: (areacode: string) => {
-      getAirQualityDataForLocation(dispatch, areacode);
-    },
-  };
-};
-
-const mapStateToProps = (state: RootState) => {
-  return {
-    AQI: state.airquality.AQI,
-    NO2_AQI: state.airquality.NO2_AQI,
-    PM10_AQI: state.airquality.PM10_AQI,
-    PM25_AQI: state.airquality.PM25_AQI,
-    currentLocation: state.locations.currentLocation,
-  };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(AirQualityScreen);
+export default AirQualityScreen;
 
 const styles = StyleSheet.create({
   carouselContainerStyle: {
